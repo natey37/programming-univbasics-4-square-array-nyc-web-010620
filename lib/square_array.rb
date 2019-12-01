50aa47027d0ffc7aeb5e78a06feb99d2d191aef2
Nathan Brickett <natey37@gmail.com>
2019-12-01 04:36:33 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  counter = 0 
+  new_array = []
+  while counter < array.size 
+    new_array << array[counter] * array[counter]
+    counter += 1
+  end 
+  new_array
+      
 end
+
+#def square_array(array)
+ # array.map{|x| x*x}
+#end 
